26ec9a47dd5c667fa7b506f77737e306f3c17df8
Donovan Duperrois <slashhable@gmail.com>
2022-06-20 19:08:20 +0200

    corrections de script, ajout des ancors

--- script/script.js
+++ script/script.js
@@ -19,23 +19,54 @@ window.addEventListener("scroll", function(){
 function retour(){
     document.documentElement.scrollTop = 0;
 }
-function presentation(){
-    document.documentElement.scrollTop = 785;
-}
-function competence(){
-    document.documentElement.scrollTop = 1800;
-}
-function projet(){
-    document.documentElement.scrollTop = 2917;
-}
-function contact(){
-    document.documentElement.scrollTop = 4360;
-}
 
-const accueil= document.getElementById("accueil")
+const accueil= document.getElementById("accBtn")
 
-function test(){
-    if(window.scrollY >=0 && window.scrollY<785){
-        accueil.className.replace("hoverUnderline","secFocus");
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY >=0 && window.scrollY<800){
+        document.getElementById("accBtn").classList.remove("hoverUnderline")
+        document.getElementById("accBtn").classList.add("secFocus")
     }
-};
+    else{
+        document.getElementById("accBtn").classList.remove("secFocus")
+        document.getElementById("accBtn").classList.add("hoverUnderline")
+ 
+    }
+    if(window.scrollY >=836 && window.scrollY<1764){
+        document.getElementById("preBtn").classList.remove("hoverUnderline")
+        document.getElementById("preBtn").classList.add("secFocus")
+    }
+    else{
+        document.getElementById("preBtn").classList.remove("secFocus")
+        document.getElementById("preBtn").classList.add("hoverUnderline")
+ 
+    }
+    if(window.scrollY >=1765 && window.scrollY<2877){
+        document.getElementById("compBtn").classList.remove("hoverUnderline")
+        document.getElementById("compBtn").classList.add("secFocus")
+    }
+    else{
+        document.getElementById("compBtn").classList.remove("secFocus")
+        document.getElementById("compBtn").classList.add("hoverUnderline")
+ 
+    }
+    if(window.scrollY >=2971 && window.scrollY<4315){
+        document.getElementById("proBtn").classList.remove("hoverUnderline")
+        document.getElementById("proBtn").classList.add("secFocus")
+    }
+    else{
+        document.getElementById("proBtn").classList.remove("secFocus")
+        document.getElementById("proBtn").classList.add("hoverUnderline")
+ 
+    }
+    if(window.scrollY >=4316 && window.scrollY<4600){
+        document.getElementById("conBtn").classList.remove("hoverUnderline")
+        document.getElementById("conBtn").classList.add("secFocus")
+    }
+    else{
+        document.getElementById("conBtn").classList.remove("secFocus")
+        document.getElementById("conBtn").classList.add("hoverUnderline")
+ 
+    }
+})  
